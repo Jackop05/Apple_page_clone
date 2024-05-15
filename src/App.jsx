d66f7fcded0b473cab9a9cde06043b0065590777
Jackop05 <jakubnyrbotzs@gmail.com>
2024-05-15 14:18:18 +0200

    Another update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
+import Model from './components/Model';
 
 
 
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
